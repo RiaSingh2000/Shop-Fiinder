d86c64fb52ea3e89715c4d2189346b7f3eda1925
TechieAditi <55248714+TechieAditi@users.noreply.github.com>
2020-03-26 21:57:45 +0530

    Seller Profile Creation has been done

--- app/src/main/java/com/codepth/maps/Welcomepage.java
+++ app/src/main/java/com/codepth/maps/Welcomepage.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,16 +21,20 @@ public class Welcomepage extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Welcomepage.this,LoginActivity.class);
+                Intent intent=new Intent(Welcomepage.this,ChoiceofLoginRole.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
+                finish();
 
             }
         });
         Signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Welcomepage.this,ChoiceofRole.class);
+                Intent intent=new Intent(Welcomepage.this, ChoiceofRegRole.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
+                finish();
             }
         });
     }
