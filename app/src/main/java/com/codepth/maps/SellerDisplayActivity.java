08b6409fdcd0567a896f745b55a38ec10c5334cb
Roli Verma <50513858+RoliVerma@users.noreply.github.com>
2020-04-05 01:14:59 +0530

    SellerDisplayActivity Uniquely Identified sellerDisplayActivity complete

--- app/src/main/java/com/codepth/maps/SellerDisplayActivity.java
+++ app/src/main/java/com/codepth/maps/SellerDisplayActivity.java
@@ -3,6 +3,7 @@ package com.codepth.maps;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,7 +26,9 @@ import java.util.List;
 public class SellerDisplayActivity extends AppCompatActivity {
 
     //ACTIVITY WIDGETS
-    LinearLayout linearLayout = null;
+    TextView tvShopName , tvSellerName , tvLocation , tvCall;
+    Button  btnChat ;
+    private ProgressDialog progressDialog;
 
     //FIREBASE
     FirebaseFirestore fstore;
@@ -40,11 +43,24 @@ public class SellerDisplayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_display);
         uid = receiveUid();
-        linearLayout=findViewById(R.id.lL_SellerDisplay);
+        initializeWidgets();
         fetchSellerInfoFromFirebase();
     }
 
+    private void initializeWidgets() {
+        btnChat = findViewById(R.id.btnChat);
+        tvCall = findViewById(R.id.tvCall);
+        tvShopName = findViewById(R.id.tvShopname);
+        tvSellerName = findViewById(R.id.tvSellerName);
+        tvLocation = findViewById(R.id.tvShopLocation);
+        progressDialog=new ProgressDialog(this);
+    }
+
     private void fetchSellerInfoFromFirebase() {
+        progressDialog.setTitle("Please Wait");
+        progressDialog.setMessage("We Are Fetching Shop Information...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
         fstore= FirebaseFirestore.getInstance();
         DocumentReference docRef = fstore.collection("Seller").document(uid);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -55,7 +71,8 @@ public class SellerDisplayActivity extends AppCompatActivity {
                     if (document.exists()) {
                         mSellerProfile = document.toObject(com.codepth.maps.mSellerProfile.class);
                         Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        setLayoutWidgets(mSellerProfile);
+                        //setLayoutWidgets(mSellerProfile);
+                        editWidgets(mSellerProfile);
                     } else {
                         Log.d(TAG, "No such document");
                     }
@@ -66,27 +83,12 @@ public class SellerDisplayActivity extends AppCompatActivity {
         });
     }
 
-    private void setLayoutWidgets(com.codepth.maps.mSellerProfile mSellerProfile) {
-        TextView textView = new TextView(this);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        );
-        textView.setText(mSellerProfile.getShopname());
-        textView.setId((int)19);
-        linearLayout.addView(textView,params);
-    }
-
-
-    private void addTextView(String text , int id) {
-        TextView textView = new TextView(this);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        );
-        textView.setText(text);
-        textView.setId(id);
-        linearLayout.addView(textView,params);
+    private void editWidgets(com.codepth.maps.mSellerProfile mSellerProfile) {
+        tvShopName.setText(mSellerProfile.getShopname());
+        tvSellerName.setText(mSellerProfile.getSelname());
+        tvLocation.setText(mSellerProfile.getLoc());
+        tvCall.setText(mSellerProfile.getCustcare());
+        progressDialog.dismiss();
     }
 
     private String receiveUid() { //gets uid of the clicked marker shop from intentExtra
@@ -95,5 +97,11 @@ public class SellerDisplayActivity extends AppCompatActivity {
         return selleruid;
     }
 
+
+    public void goToChatActivity(View view) {
+        Intent intent = new Intent(this,SellerChatActivity.class);
+        intent.putExtra("uid",mSellerProfile.getUid());
+        startActivity(intent);
+    }
 
 }
