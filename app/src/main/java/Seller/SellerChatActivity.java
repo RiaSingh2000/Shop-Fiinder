28c8d5b016660cad71c0faa8fbd4cb333a01a794
Ria <1805687@kiit.ac.in>
2020-04-13 23:32:37 +0530

    ChatList Issue Resolved

--- app/src/main/java/Seller/SellerChatActivity.java
+++ app/src/main/java/Seller/SellerChatActivity.java
@@ -67,27 +67,28 @@ public class SellerChatActivity extends AppCompatActivity {
                         )
                             buyerUid.add(snapshot.getData().get("sender").toString());
                         Toast.makeText(SellerChatActivity.this, ""+buyerUid, Toast.LENGTH_SHORT).show();
+                        db.collection("Buyer").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                list.clear();
+                                if(task.isSuccessful()){
+                                    list.clear();
+                                    for(QueryDocumentSnapshot snapshot:task.getResult()){
+                                        if(buyerUid.contains(snapshot.getData().get("uid").toString())){
+                                            list.add(new BuyerList(snapshot.getData().get("name").toString(),snapshot.getData().get("uid").toString()));
+                                            Toast.makeText(SellerChatActivity.this, ""+list, Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                    listOfBuyers.setAdapter(new BuyerListAdapter(SellerChatActivity.this,list));
+                                }
+                            }
+                        });
                          }
                 }
             }
         });
 
-        db.collection("Buyer").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                list.clear();
-                if(task.isSuccessful()){
-                    list.clear();
-                    for(QueryDocumentSnapshot snapshot:task.getResult()){
-                       if(buyerUid.contains(snapshot.getData().get("uid").toString())){
-                           list.add(new BuyerList(snapshot.getData().get("name").toString(),snapshot.getData().get("uid").toString()));
-                           Toast.makeText(SellerChatActivity.this, ""+list, Toast.LENGTH_SHORT).show();
-                    }
-                    }
-                    listOfBuyers.setAdapter(new BuyerListAdapter(SellerChatActivity.this,list));
-                }
-            }
-        });
+
         listOfBuyers.setAdapter(new BuyerListAdapter(SellerChatActivity.this,list));
 
 
